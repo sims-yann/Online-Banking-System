daaf44bba219db171859b9a32ff91238343a0d17
Takam-Ismael <ismaelulrich121@gmail.com>
2025-05-27 19:34:36 +0100

    controller 2

--- src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/serviceimpl/TransferServiceImpl.java
+++ src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/serviceimpl/TransferServiceImpl.java
@@ -2,29 +2,30 @@ package com.stjeanuniv.isi3eng2025.onlinebankingsystem.serviceimpl;
 
 import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.Account;
 import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.Transfer;
+import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.types.AccountStatus;
+import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.types.AccountType;
 import com.stjeanuniv.isi3eng2025.onlinebankingsystem.repositories.TransferRepo;
-import com.stjeanuniv.isi3eng2025.onlinebankingsystem.services.AccountService;
+import com.stjeanuniv.isi3eng2025.onlinebankingsystem.services.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-<<<<<<< Updated upstream
-import java.util.*;
-=======
->>>>>>> Stashed changes
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class TransferServiceImpl {
+@Component
+public class TransferServiceImpl implements TransferService {
+
+    private final AccountServiceImpl accountServiceImpl;
 
     @Autowired
     private final TransferRepo transferRepo;
-    private final AccountService accountService;
 
-    TransferServiceImpl(TransferRepo transferRepo, AccountService accountService){
+    TransferServiceImpl(TransferRepo transferRepo, AccountServiceImpl accountServiceImpl){
         this.transferRepo = transferRepo;
-        this.accountService = accountService;
+        this.accountServiceImpl = accountServiceImpl;
     }
 
 
@@ -32,7 +33,6 @@ public class TransferServiceImpl {
     public Map<String, Object> showTransferDetails(Transfer t){
         Map<String, Object> transferMetrics = new HashMap<>();
 
-        transferMetrics.put("code", t.getId());
         transferMetrics.put("sender", t.getSender());
         transferMetrics.put("receiver", t.getReceiver());
         transferMetrics.put("amount", t.getAmount());
@@ -42,11 +42,6 @@ public class TransferServiceImpl {
         return transferMetrics;
     }
 
-<<<<<<< Updated upstream
-    //To transfer an amount from a sender account to a receiver account
-
-=======
->>>>>>> Stashed changes
     public List<Transfer> getTransferList(List<Account> accounts) {
         List<Transfer> transferList = new ArrayList<>();
 
@@ -64,7 +59,7 @@ public class TransferServiceImpl {
     }
 
     //To tranfer an amount from a sender account to a receiver account
-   public void applyTransfer(Transfer transfer){
+    public void applyTransfer(Transfer transfer){
 
         if(senderNotReceiver(transfer.getSender(), transfer.getReceiver(), transfer.getAmount())){
             transfer.getSender().setBalance(transfer.getSender().getBalance() - transfer.getAmount());
@@ -72,13 +67,13 @@ public class TransferServiceImpl {
 
             recordTransfer(transfer);
 
-            accountService.updateAccount(transfer.getSender());
-            accountService.updateAccount(transfer.getReceiver());
+            accountServiceImpl.updateAccount(transfer.getSender());
+            accountServiceImpl.updateAccount(transfer.getReceiver());
         }
 
     }
 
-    //To verify the account balance is greater than amount
+    //To verify account balance is greater than amount
     public boolean verifyBalance(Account sender, double amount){
         if(sender.getBalance() >= amount){
             return verifyAccountType(sender);
@@ -86,20 +81,20 @@ public class TransferServiceImpl {
         return false;
     }
 
-    //To verify the account type (currant ou epargne)
+    //To verify the account type (current ou epargne)
     public boolean verifyAccountType(Account verify){
-        if(verify.getType().equals("CURRANT") ){
-            return true;
+        if(verify.getType().equals(AccountType.CURRENT) || verify.getStatus().equals(AccountStatus.ACTIVE)){
+            return false;
         }
-        return false;
+        return true;
     }
 
     //To verify that sender is not receiver
     public boolean senderNotReceiver(Account sender, Account receiver, double amount){
-        if(sender.getId() != receiver.getId()){
-            return verifyBalance(sender, amount)&&verifyAccountType(receiver);
+        if(sender.getId() == receiver.getId() || receiver.getStatus().equals(AccountStatus.BLOCK)){
+            return false;
         }
-        return false;
+        return true;
     }
 
     public void recordTransfer(Transfer transfer){
--- src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/services/AccountService.java
+++ src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/services/AccountService.java
@@ -1,29 +1,21 @@
 package com.stjeanuniv.isi3eng2025.onlinebankingsystem.services;
 
 import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.Account;
-import com.stjeanuniv.isi3eng2025.onlinebankingsystem.entities.Transfer;
 
-import java.util.List;
 import java.util.Map;
 
 public interface AccountService {
-
-    public void updateAccount(Account account);
-
     //deactivating an account
 
     public void blockAccount(int id);
 
-<<<<<<< Updated upstream
-=======
     public void updateAccount(Account account);
 
->>>>>>> Stashed changes
-    public void recordAccount(Account account);
+    public void CreateAccount(Account account);
 
     public void DeleteAccount(Account account);
 
-    public List<Account> getAccount(int id);
+    public Account getAccount(int id);
 
     public Map<String, Object> viewAccountDetails(int id);
 
