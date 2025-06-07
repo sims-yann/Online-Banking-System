183bfe6e3d0a7e5169691b444eb24cd01c69817a
sims-yann <simoyann2004@gmail.com>
2025-06-07 19:38:45 +0100

    added thymeleaf tags to customer page

--- src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/controllers/rest_controllers/AdminController.java
+++ src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/controllers/rest_controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.stjeanuniv.isi3eng2025.onlinebankingsystem.controllers.rest_controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +12,20 @@ public class AdminController {
     @GetMapping("/Dashboard")
     public String Dashboard(){
         return "/admin/admin-dashboard";
+    }
+
+    @GetMapping("/Settings")
+    public String Settings(Model model) {
+        return "/admin/admin-settings";
+    }
+
+    @GetMapping("/Transactions")
+    public String Transactions(Model model) {
+        return "/admin/admin-transaction";
+    }
+
+    @GetMapping("/Users")
+    public String Users(Model model) {
+        return "/admin/admin-users";
     }
 }
