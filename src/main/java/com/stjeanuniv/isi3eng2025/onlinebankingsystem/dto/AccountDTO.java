75231bee3410ca7af62d97b2f1e2bfd516dccac6
Takam-Ismael <ismaelulrich121@gmail.com>
2025-06-08 20:38:13 +0100

    dtos
    creating dtos

---
+++ src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/dto/AccountDTO.java
@@ -0,0 +1,8 @@
+package com.stjeanuniv.isi3eng2025.onlinebankingsystem.dto;
+
+public class AccountDTO {
+    private int userId;
+    private double accBalance;
+    private String type;
+
+}
---
+++ src/main/java/com/stjeanuniv/isi3eng2025/onlinebankingsystem/dto/TransferDTO.java
@@ -0,0 +1,8 @@
+package com.stjeanuniv.isi3eng2025.onlinebankingsystem.dto;
+
+public class TransferDTO {
+    public int sender;
+    public int receiver;
+    public double amount;
+
+}
